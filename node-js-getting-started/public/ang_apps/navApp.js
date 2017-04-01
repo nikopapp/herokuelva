9a3dc00d49c45b49003951e4eae95fa036c95e47
nikopapp <np15685@my.bristol.ac.uk>
2017-04-01 15:06:17 +0100

    fixed header in dumb pages

--- node-js-getting-started/public/ang_apps/navApp.js
+++ node-js-getting-started/public/ang_apps/navApp.js
@@ -1,4 +1,32 @@
 angular.module("navApp", []);
-angular.module("navApp").controller("navCtrl", ["$scope", function(scope){
 
+angular.module("navApp").controller("navCtrl", ["$scope", function(scope) {
+ var self = this;
+ self.languageObj = {
+   value: "ENG"
+ };
+ self.menuENG = {
+    artwork: "ARTWORK",
+    paintings: "PAINTINGS",
+    mix_tech: "MIXED TECHNIQUES",
+    islamic: "ISLAMIC ART"
+ }
+ self.menuESP = {
+    artwork: "OBRAS",
+    paintings: "PINTURA",
+    mix_tech: "TECNICAS MIXTAS",
+    islamic: "ARTE ISLAMICA"
+ }
+
+  self.menu = self.menuENG;
+  self.setLangESP = function(){
+    console.log("changed");
+    self.languageObj.value = "ESP";
+    self.menu = self.menuESP;
+  }
+  self.setLangENG = function(){
+    console.log("changed");
+    self.languageObj.value = "ENG";
+    self.menu = self.menuENG;
+  }
 }]);
