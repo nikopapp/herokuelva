5f0cc5aa992415722d26ae744b2909f9a209268e
Nikolaos Pappas <nikolaospappa@gmail.com>
2018-12-07 20:52:47 +0200

    photos swap

--- node-js-getting-started/database/setup/create.js
+++ node-js-getting-started/database/setup/create.js
@@ -70,20 +70,20 @@ var paintings = {
         new Item("21",   "Angeliki",      "" ,"angeliki.jpg",    "Acrílico sobre tela, 61x 91 cm, 2017", "Acrylic on canvas, 61x 91 cm"," 2018", "TODO"),
         new Item("20",   "Sara",      "" ,"sara.jpg",    "Acrílico sobre tela, 61x 91 cm, 2017", "Acrylic on canvas, 61x 91 cm"," 2018", "TODO"),
         new Item("19",   "La familia",      "" ,"Family.jpg",    "Acrílico sobre tela, 61x 91 cm, 2017", "Acrylic on canvas, 61x 91 cm"," 2018", "TODO"),
-        new Item("18",  "Los cinco",        "" ,"k19.jpg",   "Acrílico y esmalte sintético sobre tabla, 160x 80 cm, 2006","Acrylic and synthetic enamel on woodboard, 160x 80 cm"," 2006", "c19.jpg"),
-        new Item("17",   "Xela y Berni",     "" ,"k4.jpg",    "Acrílico sobre tela, 120x 40 cm, 2014",                    "Acrylic on canvas, 120x 40 cm"," 2014", "c4.jpg"),
-        new Item("16",  "El recreo",        "" ,"k16.jpg",   "Acrílico y esmalte sintético sobre tabla, 160x 120 cm, 2006","Acrylic and synthetic enamel on woodboard, 160x 120 cm"," 2006", "c16.jpg"),
+        new Item("18",  "Los cinco",        "" ,"los-cinco.jpg",   "Acrílico y esmalte sintético sobre tabla, 160x 80 cm, 2006","Acrylic and synthetic enamel on woodboard, 160x 80 cm"," 2006", "c19.jpg"),
+        new Item("17",   "Xela y Berni",     "" ,"xela.jpg",    "Acrílico sobre tela, 120x 40 cm, 2014",                    "Acrylic on canvas, 120x 40 cm"," 2014", "c4.jpg"),
+        new Item("16",  "El recreo",        "" ,"recreo.jpg",   "Acrílico y esmalte sintético sobre tabla, 160x 120 cm, 2006","Acrylic and synthetic enamel on woodboard, 160x 120 cm"," 2006", "c16.jpg"),
         new Item("15",  "Santiago",         "" ,"k17.jpg",   "Esmalte sintético sobre tabla, 70x 32 cm, 2005",          "Synthetic enamel on woodboard, 70x 32 cm"," 2005", "c17.jpg"),
-        new Item("14",  "Murgartegui",      "" ,"k18.jpg",   "Esmalte sintético sobre tabla, 100x 160 cm, 2006",        "Synthetic enamel on woodboard, 100x 160 cm"," 2006", "c18.jpg"),
-        new Item("13",  "La charla",        "" ,"k11.jpg",   "Acrílico sobre tela, 43x 58 cm, 2008",                     "Acrylic on canvas, 43x 58 cm"," 2008", "c11.jpg"),
-        new Item("12",   "Autorretrato",  "margin-mid" ,"k6.jpg",    "Esmalte sintético sobre tabla, 60x 40 cm, 2006",            "Synthetic enamel on woodboard, 60x 40 cm"," 2006", "c6.jpg"),
-        new Item("11", "Pilar",           "",     "k3.jpg", "Esmalte sintético sobre tabla, 25x 33 cm, 2006", "Synthetic enamel on woodboard, 25x 33 cm"," 2006", "c3.jpg"),
+        new Item("14",  "Murgartegui",      "" ,"mugartegui.jpg",   "Esmalte sintético sobre tabla, 100x 160 cm, 2006",        "Synthetic enamel on woodboard, 100x 160 cm"," 2006", "c18.jpg"),
+        new Item("13",  "La charla",        "" ,"the-chat.jpg",   "Acrílico sobre tela, 43x 58 cm, 2008",                     "Acrylic on canvas, 43x 58 cm"," 2008", "c11.jpg"),
+        new Item("12",   "Autorretrato",  "margin-mid" ,"selfportrait.jpg",    "Esmalte sintético sobre tabla, 60x 40 cm, 2006",            "Synthetic enamel on woodboard, 60x 40 cm"," 2006", "c6.jpg"),
+        new Item("11", "Pilar",           "",     "pili.jpg", "Esmalte sintético sobre tabla, 25x 33 cm, 2006", "Synthetic enamel on woodboard, 25x 33 cm"," 2006", "c3.jpg"),
         new Item("10",  "La estación",      "" ,"k25.jpg",   "Esmalte sintético sobre tabla, 25x 33 cm, 2004",          "Synthetic enamel on woodboard, 25x 33 cm"," 2004", "c25.jpg"),
         new Item("9",  "Encuentros",       "" ,"k22.jpg",   "Esmalte sintético sobre tabla , 25x 33 cm, 2004",         "Synthetic enamel on woodboard, 25x 33 cm"," 2004", "c22.jpg"),
-        new Item("8",  "A Rúa",            "" ,"k24.jpg",   "Esmalte sintético sobre tabla, 25x 33 cm, 2004",          "Synthetic enamel on woodboard, 25x 33 cm"," 2004", "c24.jpg"),
+        new Item("8",  "A Rúa",            "" ,"rua.jpg",   "Esmalte sintético sobre tabla, 25x 33 cm, 2004",          "Synthetic enamel on woodboard, 25x 33 cm"," 2004", "c24.jpg"),
         new Item("7",  "La playa",         "" ,"k20.jpg",   "Esmalte sintético sobre tabla, 25x 33 cm, 2004",          "Synthetic enamel on woodboard, 25x 33 cm"," 2004", "c20.jpg"),
         new Item("6",  "La playa II",      "" ,"k21.jpg",   "Esmalte sintético sobre tabla, 100x 50 cm, 2005",         "Synthetic enamel on woodboard, 100x 50 cm"," 2004", "c21.jpg"),
-        new Item("1",   "Zu",               "" ,"k7.jpg",    "Acrílico sobre tela, 27x 43 cm, 2009",                     "Acrylic on canvas, 27x 43 cm"," 2009", "c7.jpg"),
+        new Item("1",   "Zu",               "" ,"zu.jpg",    "Acrílico sobre tela, 27x 43 cm, 2009",                     "Acrylic on canvas, 27x 43 cm"," 2009", "c7.jpg"),
     ]
 };
 
@@ -91,12 +91,12 @@ var mix_tech = {
  items:[
     new Item("8", "Yoli", "","yoli.jpg","Collage, 46x 61 cm, 2009","Collage, 46x 61 cm"," 2018", "TODO"),
     new Item("7",  "Mira en ti","","k13.jpg","Acrílico sobre tela, 100x 100 cm, 2001", "Acrylic on canvas, 100x 100 cm"," 2004", "c13.jpg"),
-    new Item("6", "El tiempo II", "","t5.jpg","Collage sobre tabla, 46x 61 cm, 2010", "Collage on woodboard, 46x 61 cm"," 2010","p6.jpg"),
-    new Item("5","Donna I" , "margin-mid","t6.jpg","Collage sobre tabla, 30x 70 cm, 2011","Collage on woodboard, 30x 70 cm"," 2010","p5.jpg"),
+    new Item("6", "El tiempo II", "","time.jpg","Collage sobre tabla, 46x 61 cm, 2010", "Collage on woodboard, 46x 61 cm"," 2010","p6.jpg"),
+    new Item("5","Donna I" , "margin-mid","Donna I.jpg","Collage sobre tabla, 30x 70 cm, 2011","Collage on woodboard, 30x 70 cm"," 2010","p5.jpg"),
     new Item("4", "Las tres Marías", "","p9.jpg","Collage sobre A4, 2007","Collage on A4"," 2007","p9.jpg"),
-    new Item("3", "Tu pensamiento te hace libre", "","t1.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p1.jpg"),
-    new Item("2", "El hambre", "","t2.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p2.jpg"),
-    new Item("1", "La espera", "","t3.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p3.jpg"),
+    new Item("3", "Tu pensamiento te hace libre", "","tu pensamiento te hace libre.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p1.jpg"),
+    new Item("2", "El hambre", "","el hambre.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p2.jpg"),
+    new Item("1", "La espera", "","la espera.jpg","Collage sobre A4, 2011", "Collage on A4"," 2011","p3.jpg"),
   ]
 };
 
